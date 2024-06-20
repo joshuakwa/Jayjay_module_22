5d39cf74b225087d4131da24b90382623f555515
Joshua <joshuakwa99@gmail.com>
2024-06-20 16:31:55 +0700

    add more ui automation

---
+++ UI Automation/test_all_filter_on_homepage.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def test_all_filter_on_homepage():
+    o = Options()
+    # o.add_argument("--headless")
+    o.add_argument("--start-maximized")
+    o.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=o)
+    driver.get("https://www.saucedemo.com/")
+    driver.maximize_window()
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    # assert user success login and in homepage
+    driver.find_element(By.ID, "item_4_title_link").is_displayed()
+
+    driver.find_element(By.CLASS_NAME, "product_sort_container").click()
+    driver.find_element(By.CSS_SELECTOR, "option[value='za']").click()
+
+    time.sleep(3)
+    driver.quit()
